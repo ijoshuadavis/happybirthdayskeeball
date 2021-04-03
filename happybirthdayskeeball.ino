8ee0896ac814802065e6fbbbeced75c477ba1c36
Joshua L. Davis <988414+ijoshuadavis@users.noreply.github.com>
2021-04-03 10:12:22 -0400

    Stub 90% complete and initialize score and game start buttons

--- happybirthdayskeeball.ino
+++ happybirthdayskeeball.ino
@@ -7,9 +7,7 @@ Date:
 Description:
 
 Technical Specs:
-  Arduino model - version...blah
-
-
+  Arduino model - version...blah blah
 
 
   
@@ -28,16 +26,33 @@ Technical Specs:
 //****************************************************************************//
 // Constants & Predefines
 //****************************************************************************//
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int pinBallButton10 = 2;      // the number of the pushbutton pin
+const int pinBallButton20 = 3;      // the number of the pushbutton pin
+const int pinBallButton30 = 4;      // the number of the pushbutton pin
+const int pinBallButton40 = 5;      // the number of the pushbutton pin
+const int pinBallButton50 = 6;      // the number of the pushbutton pin
+const int pinBallButton100Left = 7; // the number of the pushbutton pin
+const int pinBallButton100Right = 8;// the number of the pushbutton pin
+const int pinGameResetButton = 9;   // the number of the pushbutton pin
+const int pinBallDoorMotor = 10;    // the number of the motor pin
+const int pinScoreboardHigh = 11;   // the number of the Scoreboard High pin
+const int pinScoreboardCurrent = 12;// the number of the Scoreboard Current pin
+const int ledPin =  13;             // the number of the LED pin
 
 
 //****************************************************************************//
 // Varaibles
 //****************************************************************************//
-int buttonState = 0;         // variable for reading the pushbutton status
-int currentScore = 0;        // variable for keeping track of the current game's score
-int currentHighScore = 0;    // varialbe pulled from EEPROM to see current high score
+int stateBallButton10 = 0;          //
+int stateBallButton20 = 0;          // 
+int stateBallButton30 = 0;          // 
+int stateBallButton40 = 0;          //
+int stateBallButton50 = 0;          //
+int stateBallButton100Left = 0;     //
+int stateBallButton100Right = 0;    // 
+int stateGameResetButton = 0;       // 
+int currentScore = 0;               // variable for keeping track of the current game's score
+int currentHighScore = 0;           // varialbe pulled from EEPROM to see current high score
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -88,10 +103,23 @@ void UpdateScoreboard() {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// GAME RESTART METHODS
+// BALLDOOR METHODS
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
+//****************************************************************************//
+// LowerBalldoor
+//****************************************************************************//
+void LowerBalldoor() {
+  
+}
+
+//****************************************************************************//
+// RaiseBalldoor
+//****************************************************************************//
+void RaiseBalldoor() {
+  
+}
 
 
 
@@ -124,7 +152,7 @@ void CheckHighScore() {
 
 
 //****************************************************************************//
-// IncementScore - 
+// IncrementScore - 
 //****************************************************************************//
 void IncrementScore( int ballDropButton ) {
 
@@ -151,14 +179,27 @@ void IncrementScore( int ballDropButton ) {
 // Setup - Initialize all buttons, scoreboard, and open / close ball door
 //****************************************************************************//
 void setup() {
-  
+
+  ////////////////////////////////////////
+  ////////DEBUGGING initialize the LED pin
+  pinMode(ledPin, OUTPUT);
+
   // pull current high score from EEPROM and set to global currentHighScore variable
 
   // setup score buttons
-
+  pinMode(pinBallButton10, INPUT);      // initialize the pushbutton pin as an input
+  pinMode(pinBallButton20, INPUT);      // initialize the pushbutton pin as an input
+  pinMode(pinBallButton30, INPUT);      // initialize the pushbutton pin as an input
+  pinMode(pinBallButton40, INPUT);      // initialize the pushbutton pin as an input
+  pinMode(pinBallButton50, INPUT);      // initialize the pushbutton pin as an input
+  pinMode(pinBallButton100Left, INPUT); // initialize the pushbutton pin as an input
+  pinMode(pinBallButton100Right, INPUT);// initialize the pushbutton pin as an input
+  
   // setup scoreboard and feedback
 
   // setup game restart button & raise ball-door
+  /// might need to pause the raise door to give the balls enough time to roll down
+  pinMode(pinGameResetButton, INPUT);   // initialize the pushbutton pin as an input
   
 }
 
@@ -167,8 +208,8 @@ void setup() {
 //****************************************************************************//
 void loop() {
 
-  // check game restart button, lower ball-door, then reset Arduino
+  // check game restart button - lower ball-door and then reset Arduino
  
-  // check ball drop button and increment score
+  // check balldrop button - increment score
 
 }
